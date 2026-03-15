32895d7e68f6340b956c5ffedd3f6da94440b416
Karim <karimbouazizpip@gmail.com>
2026-03-15 13:12:43 +0100

    Integrate Gemini model to summarize the news for email content

--- main.py
+++ main.py
@@ -2,39 +2,57 @@ import requests
 import os
 from dotenv import load_dotenv
 
+from langchain.chat_models import init_chat_model
+
 from send_email import send_email
 
 load_dotenv()
 
+# Load API keys from environment variables
 API_KEY = os.getenv("NEWS_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-topic = "tesla"
+topic = "business"
 
 url = (
     f"https://newsapi.org/v2/everything?"
     f"q={topic}&"
-    f"from=2026-02-08&"
+    f"from=2026-02-15&"
     f"sortBy=publishedAt&"
     f"apiKey={API_KEY}&"
     f"language=en"
 )
 # Make a GET request to the API
-response = requests.get(url)
+request = requests.get(url)
 
 # Get the JSON content from the response
-content = response.json()
+content = request.json()
 
-email_content = ""
+# Extract the articles from the content
+articles = content["articles"]
 
-for article in content["articles"]:
-    email_content += (
-        f"Title: {article['title']}\n"
-        f"Description: {article['description']}\n"
-        f"URL: {article['url']}\n\n"
-    )
+# Initialize the Gemini model
+model = init_chat_model(
+    model="gemini-3-flash-preview", 
+    model_provider="google-genai",
+    api_key = GEMINI_API_KEY
+)
 
+prompt = f"""
+You are a news summarizer. 
+Write a short paragraph analyzing those news.
+And add another second paragraph to tell me how they affect 
+the stock market.
+Here are news articles:
+{articles}
+"""
 
+# Invoke the model with the prompt and get the response
+response = model.invoke(prompt)
+response_str = response.content[0]["text"]
+
+# Send the response via email
 send_email(
-    message=email_content,
-    subject="Daily News Update"
+    message=response_str,
+    subject=f"Daily {topic.capitalize()} News Update"
 )
